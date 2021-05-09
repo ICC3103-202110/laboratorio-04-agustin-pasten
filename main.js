a05ef5cbced08e5fdbef7bf0b26ba071966fb74e
agustin-pasten <aopasten@miuandes.cl>
2021-05-09 01:26:56 -0400

    posible termino de codigo

---
+++ main.js
@@ -0,0 +1,32 @@
+const prompt = require('prompt-sync')();
+
+function view(counter){
+    return counter
+}
+
+function update(msg,counter){
+    if (msg ==="+")
+        counter = counter +1;
+    if (msg ==="-")
+        counter = counter -1
+    return counter
+}
+    
+function app(counter){
+    console.clear()
+    console.log("counter:",view(counter));
+    console.log("(+)   (-)");
+    console.log("(q) for quit");
+    let action = prompt('What would you do?: ');
+    if (action ==="q")
+        console.log();
+    else{
+        counter = update(action,counter);
+        app(counter);
+    }
+
+}
+
+//let action = prompt('What would you do?: ');
+
+app(0)
